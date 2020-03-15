8f9f3a4344d542f3c0b9555ba7df8da21c9894ca
sidashen <shashasi8023@163.com>
2020-03-15 17:15:11 +0800

    [shasha] feat: show/add button in App component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { isShow: false };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState(state => ({
+      isShow: !state.isShow
+    }));
+  }
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.handleClick}>
+          {this.state.isShow ? 'Hide' : 'Show'}
+        </button>
+        <div>{this.state.isShow && <ToDoList />}</div>
+      </div>
+    );
+  }
 }
 
 export default App;
+
